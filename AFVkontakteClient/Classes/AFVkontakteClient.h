002bbbf83eca02e46af3dfabbc6c505a9d66ad2b
Anton Domashnev <antondomashnev@gmail.com>
2013-10-07 23:49:26 +0400

    Add ADVkontakteClient calss;

---
+++ AFVkontakteClient/Classes/AFVkontakteClient.h
@@ -0,0 +1,13 @@
+//
+//  AFVkontakteClient.h
+//  AFVkontakteClient
+//
+//  Created by Антон Домашнев on 07.10.13.
+//
+//
+
+#import <AFNetworking/AFHTTPRequestOperationManager.h>
+
+@interface AFVkontakteClient : AFHTTPRequestOperationManager
+
+@end
---
+++ AFVkontakteClient/Classes/AFVkontakteClient.m
@@ -0,0 +1,13 @@
+//
+//  AFVkontakteClient.m
+//  AFVkontakteClient
+//
+//  Created by Антон Домашнев on 07.10.13.
+//
+//
+
+#import "AFVkontakteClient.h"
+
+@implementation AFVkontakteClient
+
+@end
